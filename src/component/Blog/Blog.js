3f894f83c715a22fe09f5948797e8e902bd79c6e
arafathosain18 <hosainarafat81@gmail.com>
2022-04-19 01:00:24 +0600

    after the q added

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -5,6 +5,7 @@ const Blog = () => {
         <div>
             <h2>What is Context API</h2>
             <p>The real goal of Context API allow global variables to be passed around . This is a alternative to prop drilling .context is light and easier to use . When you use it you have to share data with a lots of components. You can also use nested very own ! </p>
+            <h2>What is semantic tag !</h2>
 
         </div>
     );
